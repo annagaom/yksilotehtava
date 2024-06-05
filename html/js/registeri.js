18838bfc90a9a660e5312111ef9c4c5152a41848
annagaom <122282732+annagaom@users.noreply.github.com>
2024-06-05 10:01:47 +0300

    registeri toimi

--- html/js/registeri.js
+++ html/js/registeri.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 function getSelectedLanguage() {
@@ -7,164 +6,67 @@ function getSelectedLanguage() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const selectedLanguage = getSelectedLanguage();
+  const selectedLanguage = getSelectedLanguage();
+  const registerButton = document.getElementById('registeriButton');
 
+  if (registerButton) {
+    registerButton.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const registeriForm = document.getElementById('registeriForm');
+      if (registeriForm && registeriForm.checkValidity()) {
+        const formData = new FormData(registeriForm);
 
-    const registerButton = document.getElementById('registerButton');
+        for (const [key, value] of formData.entries()) {
+            console.log(key, value);
+        }
 
-        registerButton.addEventListener('click', async (event) => {
-            event.preventDefault();
-            const registeriForm = document.getElementById('registrationForm');
-            if (registeriForm && registeriForm.checkValidity()) {
-                const formData = new FormData(registeriForm);
+        try {
+          const response = await fetch('http://localhost:3000/api/v1/users', {
+            method: 'POST',
+            body: formData
+          });
 
-                for (const [key, value] of formData.entries()) {
-                    console.log(key, value);
-                }
+          console.log('Response status: ', response.status);
 
-                try {
-                    const response = await fetch('http://localhost:3000/api/v1/users', {
-                        method: 'POST',
-                        body: formData
-                    });
+          if (response.ok) {
+            const data = await response.json();
+            console.log('Server response data: ', data);
 
-                    if (response.ok) {
-                        const data = await response.json();
-                        const token = data.token;  // Hakee JWT-tokenin palvelimen vastauksesta
+              let targetPage = '';  // Määrittää sivun, jonne siirrytään rekisteröinnin jälkeen
+              switch (selectedLanguage) {
+                case 'EN':
+                  alert('Registration successful. Welcome!');
+                  targetPage = '../en/login_en.html';
+                  break;
+                case 'FI':
+                default:
+                  alert('Rekisteröinti onnistui. Pääset kirjautumaan sisään!');
+                  targetPage = '../fi/login.html';
+                  break;
+              }
+              window.location.href = targetPage;  // Uudelleenohjaus rekisteröinnin jälkeen
 
-                        if (token) {
-                            localStorage.setItem('authToken', token);
-
-                            let targetPage = '';  // Määrittää sivun, jonne siirrytään rekisteröinnin jälkeen
-                            switch (selectedLanguage) {
-                                case 'EN':
-                                    alert('Registration successful. Welcome!');
-                                    targetPage = '../en/login_en.html';
-                                    break;
-                                case 'FI':
-                                default:
-                                    alert('Rekisteröinti onnistui. Pääset kirjautumaan sisään!');
-                                    targetPage = '../fi/login.html';
-                                    break;
-                            }
-                            window.location.href = targetPage;  // Uudelleenohjaus rekisteröinnin jälkeen
-                        } else {
-                            throw new Error('Registration token missing.');  // Heittää virheen, jos token puuttuu
-                        }
-                    } else {
-                        throw new Error('Registration failed');
-                    }
-                } catch (error) {
-                    console.error('Virhe rekisteröinnissä: ', error);
-                    // Käsittelee virheen kielen perusteella
-                    switch (selectedLanguage) {
-                        case 'EN':
-                            alert('An error occurred during registration. Please try again later.');
-                            break;
-                        case 'FI':
-                        default:
-                            alert('Virhe rekisteröinnissä. Yritä myöhemmin uudelleen.');
-                            break;
-                    }
-                }
-            } else {
-                console.log('Form is invalid');
-            }
-        });
-
+          } else {
+            const errorText = await response.text();
+            console.error('Registration failed: ', errorText);
+            throw new Error('Registration failed');
+          }
+        } catch (error) {
+          console.error('Virhe rekisteröinnissä: ', error);
+          // Käsittelee virheen kielen perusteella
+          switch (selectedLanguage) {
+            case 'EN':
+              alert('An error occurred during registration. Please try again later.');
+              break;
+            case 'FI':
+            default:
+              alert('Virhe rekisteröinnissä. Yritä myöhemmin uudelleen.');
+              break;
+          }
+        }
+      } else {
+        console.log('Form is invalid');
+      }
+    });
+  }
 });
-
-
-      // event.preventDefault();
-      // const firstname = document.getElementById('firstname').value || '' ;
-      // const lastname = document.getElementById('lastname').value || '' ;
-      // const photo = document.getElementById('photo').files[0] || null ;
-      // const username = document.getElementById('username').value || '' ;
-      // const password = document.getElementById('password').value || '' ;
-      // const email = document.getElementById('email').value|| '' ;
-
-    //   const data = {
-    //     firstname: firstname,
-    //     lastname: lastname,
-    //     photo: photo,
-    //     username: username,
-    //     password: password,
-    //     email: email
-    //   };
-
-    // console.log("Data: ", data);
-
-    //file upload in form data
-    // const formData = new FormData();
-    //   formData.append('firstname', firstname);
-    //   formData.append('lastname', lastname);
-    //   if (photo) {
-    //     formData.append('photo', photo);
-    //   }
-    //   formData.append('username', username);
-    //   formData.append('password', password);
-    //   formData.append('email', email);
-
-    //   console.log("Form data: ", formData);
-
-
-//       fetch('http://localhost:3000/api/v1/users', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(data),
-
-//       })
-//       .then(response => {
-//         if (response.ok) {
-//           return response.json();
-//         } else {
-//           throw new Error('Registration failed');
-//         }
-//       })
-//       .then(data => {
-//         const token = data.token;  // Hakee JWT-tokenin palvelimen vastauksesta
-
-
-//           if (token) {
-//             localStorage.setItem('authToken', token);
-
-//             let targetPage = '';  // Määrittää sivun, jonne siirrytään rekisteröinnin jälkeen
-//             switch (selectedLanguage) {
-//                 case 'EN':
-//                     alert('Registration successful. Welcome!');
-//                     targetPage = '../en/login_en.html';
-//                     break;
-//                 case 'FI':
-//                 default:
-//                     alert('Rekisteröinti onnistui. Pääset kirjautumaan sisään!');
-//                     targetPage = '../fi/login.html';
-//                     break;
-//               }
-//                 window.location.href = targetPage;  // Uudelleenohjaus rekisteröinnin jälkeen
-//             } else {
-//                 throw new Error('Registration token missing.');  // Heittää virheen, jos token puuttuu
-//           }
-//         })
-//           .catch(error => {
-//             console.error('Virhe rekisteröinnissä: ', error);
-//             // Käsittelee virheen kielen perusteella
-//             switch (selectedLanguage) {
-//               case 'EN':
-//                   alert('An error occurred during registration. Please try again later.');
-//                   break;
-//               case 'FI':
-//               default:
-//                   alert('Virhe rekisteröinnissä. Yritä myöhemmin uudelleen.');
-//                   break;
-//                   }
-//               });
-//         }
-//       });
-//     }
-//   }
-// );
-
-
-
